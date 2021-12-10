3f43bf7f9d403c61a4a6a96466b4eff18bd1e2bb
firas <firastayeb2@gmail.com>
2021-12-10 10:15:36 +0100

    init

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log('Hello world!');
+var test1;
+test1 = "a";
+var bla = 55;
+var blabla = 100;
+var a = "";
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log('Hello world!');
+var test1: string;
+test1 = "a";
+var bla = 55;
+var blabla= 100;
+var a = "";
